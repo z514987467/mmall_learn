55a886fc3d97e32622e5e172f68bc2b222e3f313
zhangmingzheng <zhangmingzheng@jd.com>
2019-03-21 18:40:45 +0800

    用户模块开发

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.mmall.service;
+
+/**
+ * @author zhangmingzheng
+ * @description
+ * @date 2019/3/21
+ **/
+public interface IUserService {
+    Object login(String username,String password);
+}
---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mmall.service.impl;
+
+import com.mmall.service.IUserService;
+
+/**
+ * @author zhangmingzheng
+ * @description
+ * @date 2019/3/21
+ **/
+public class UserServiceImpl implements IUserService {
+    @Override
+    public Object login(String username, String password) {
+        return null;
+    }
+}
