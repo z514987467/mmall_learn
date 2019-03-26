d919e32b72c4baf0ceada4b406b4f179680f2a25
zhangmingzheng <zhangmingzheng@jd.com>
2019-03-26 15:09:43 +0800

    商品品类维护

---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,21 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * @author zhangmingzheng
+ * @description
+ * @date 2019/3/26
+ **/
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+}
