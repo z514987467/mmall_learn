120a6430719fa256939bb8cd31497a301098019b
zhangmingzheng <zhangmingzheng@jd.com>
2019-04-09 17:56:19 +0800

    添加商品管理

---
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityIntercepter.java
@@ -0,0 +1,30 @@
+package com.mmall.controller.common.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author zhangmingzheng
+ * @description
+ * @date 2019/4/1
+ **/
+public class AuthorityIntercepter implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
 <html>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 <body>
 <h2>Hello World!</h2>
+
+springmvc上传文件
+<form name="form1" action="/manage/product/upload.do" method="post" , enctype="multipart/form-data">
+    <input type="file" name="upload_file"/>
+    <input type="submit" value="springmvc上传文件"/>
+</form>
 </body>
 </html>
